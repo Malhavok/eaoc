58d6e1569be844fbafe0cf5d6a141830fe5f5cac
Malhavok <malhavok@origami-dragon.com>
2025-12-03 15:30:13 +0100

    Day 3, 2025, part2 done

--- 2025/3/main.ex
+++ 2025/3/main.ex
@@ -13,21 +13,35 @@ defmodule Main do
     get_largest_digit(tail, stop_at_tail_length, largest_info)
   end
 
-  def get_largest_number(number_string) do
+  def get_largest_number(_digits, gathered, expected_length)
+      when length(gathered) == expected_length do
+    gathered |> Enum.reverse() |> Integer.undigits()
+  end
+
+  def get_largest_number(digits, gathered, expected_length) do
+    {largest_digit, tail} =
+      get_largest_digit(digits, expected_length - length(gathered) - 1, {-1, []})
+
+    get_largest_number(tail, [largest_digit | gathered], expected_length)
+  end
+
+  def get_largest_number(number_string, iterations) do
     digits = number_string |> String.to_integer() |> Integer.digits()
-    {largest_digit, tail} = get_largest_digit(digits, 1, {-1, []})
-    {next_digit, _tail} = get_largest_digit(tail, 0, {-1, []})
-    [largest_digit, next_digit] |> Integer.undigits()
+    largest_number = get_largest_number(digits, [], iterations)
+    largest_number
   end
 
   def part1(input_data) do
     lines =
       input_data |> String.split("\n") |> Enum.filter(fn elem -> String.length(elem) > 0 end)
 
-    {:ok, for(elem <- lines, do: get_largest_number(elem)) |> Enum.sum()}
+    {:ok, for(elem <- lines, do: get_largest_number(elem, 2)) |> Enum.sum()}
   end
 
-  def part2(_input_data) do
-    {:error, :notimplemented}
+  def part2(input_data) do
+    lines =
+      input_data |> String.split("\n") |> Enum.filter(fn elem -> String.length(elem) > 0 end)
+
+    {:ok, for(elem <- lines, do: get_largest_number(elem, 12)) |> Enum.sum()}
   end
 end
