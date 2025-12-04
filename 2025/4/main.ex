8b21ad03aa9028a817d202807af668aba91caae9
Malhavok <malhavok@origami-dragon.com>
2025-12-04 14:54:48 +0100

    Day 4, 2025, part2 done

--- 2025/4/main.ex
+++ 2025/4/main.ex
@@ -40,7 +40,59 @@ defmodule Main do
     {:ok, _counter} = count_neighbours(Map.keys(grid), grid, 0)
   end
 
-  def part2(_input_data) do
-    {:error, :notimplemented}
+  defp fetch_important_entries([], _grid, result) do
+    {:ok, result}
+  end
+
+  defp fetch_important_entries([point | tail], grid, result) do
+    case Map.get(grid, point) do
+      ?@ -> fetch_important_entries(tail, grid, [point | result])
+      _ -> fetch_important_entries(tail, grid, result)
+    end
+  end
+
+  defp clear_grid(grid, []) do
+    {:ok, grid}
+  end
+
+  defp clear_grid(grid, [point | tail]) do
+    new_grid = Map.put(grid, point, ?.)
+    clear_grid(new_grid, tail)
+  end
+
+  defp remove_nonlocked_entries(_grid, [], to_remove_list, still_important_list) do
+    {:ok, to_remove_list, still_important_list}
+  end
+
+  defp remove_nonlocked_entries(grid, [point | tail], to_remove_list, still_important_list) do
+    case count_from_position(point, ?@, grid) do
+      {:ok, 1} ->
+        remove_nonlocked_entries(grid, tail, [point | to_remove_list], still_important_list)
+
+      _ ->
+        remove_nonlocked_entries(grid, tail, to_remove_list, [point | still_important_list])
+    end
+  end
+
+  defp step_non_locked_entries(grid, move_points) do
+    {:ok, removed_list, new_move_points} = remove_nonlocked_entries(grid, move_points, [], [])
+    {:ok, new_grid} = clear_grid(grid, removed_list)
+    {:ok, new_grid, removed_list, new_move_points}
+  end
+
+  defp iterate_non_locked_entries(grid, move_points, counter) do
+    {:ok, new_grid, remove_list, new_move_points} = step_non_locked_entries(grid, move_points)
+    remove_list_count = length(remove_list)
+
+    case remove_list_count do
+      0 -> {:ok, counter}
+      _ -> iterate_non_locked_entries(new_grid, new_move_points, counter + remove_list_count)
+    end
+  end
+
+  def part2(input_data) do
+    {:ok, grid} = Grid.init(input_data)
+    {:ok, paper_roll_points} = fetch_important_entries(Map.keys(grid), grid, [])
+    iterate_non_locked_entries(grid, paper_roll_points, 0)
   end
 end
