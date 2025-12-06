e56e1a67dc67d4f0f1ee6b721ea2fa24ff5e8865
Malhavok <malhavok@origami-dragon.com>
2025-12-06 19:25:05 +0100

    Day 6, 2025, part2 done
    Main issue was with stripping whitespaces in tests :p

--- 2025/6/main.ex
+++ 2025/6/main.ex
@@ -28,13 +28,11 @@ defmodule Main do
   end
 
   defp apply_operation(["+" | tail]) do
-    result = perform_summation(tail, 0)
-    result
+    perform_summation(tail, 0)
   end
 
   defp apply_operation(["*" | tail]) do
-    result = perform_multiplication(tail, 1)
-    result
+    perform_multiplication(tail, 1)
   end
 
   def part1(input_data) do
@@ -43,7 +41,79 @@ defmodule Main do
     {:ok, result}
   end
 
-  def part2(_input_data) do
-    {:error, :notimplemented}
+  defp gather_offsets([], _offset, offsets) do
+    # head here will always be 0, and we're not splitting explicitly at zero.
+    [_head | tail] = offsets |> Enum.reverse()
+    {:ok, tail |> Enum.reverse()}
+  end
+
+  defp gather_offsets([?\s | tail], offset, offsets) do
+    gather_offsets(tail, offset + 1, offsets)
+  end
+
+  defp gather_offsets([_ | tail], offset, offsets) do
+    gather_offsets(tail, offset + 1, [offset | offsets])
+  end
+
+  defp read_split_offsets([last_line | _]) do
+    last_line |> String.to_charlist() |> gather_offsets(0, [])
+  end
+
+  defp split_on_offset(line, [], parts) do
+    [line | parts]
+  end
+
+  defp split_on_offset(line, [offset | rest], parts) do
+    {prefix, new_part} = line |> String.split_at(offset)
+    split_on_offset(prefix, rest, [new_part | parts])
+  end
+
+  defp split_on_offsets(line, offsets) do
+    split_on_offset(line, offsets, [])
+  end
+
+  defp convert_tail(tail) do
+    tail
+    |> Enum.reverse()
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.map(&List.to_string/1)
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(fn elem -> elem |> String.length() > 0 end)
+  end
+
+  defp apply_operation_2(["+" <> _ | tail]) do
+    new_tail = convert_tail(tail)
+    perform_summation(new_tail, 0)
+  end
+
+  defp apply_operation_2(["*" <> _ | tail]) do
+    new_tail = convert_tail(tail)
+    perform_multiplication(new_tail, 1)
+  end
+
+  defp apply_operation_2(arg) do
+    arg |> inspect() |> IO.puts()
+  end
+
+  def part2(input_data) do
+    data_lines =
+      input_data
+      |> String.split("\n")
+      |> Enum.filter(fn elem -> String.length(elem) > 0 end)
+
+    {:ok, split_offsets} = data_lines |> Enum.reverse() |> read_split_offsets()
+
+    result =
+      data_lines
+      |> Enum.map(fn line -> split_on_offsets(line, split_offsets) end)
+      |> Enum.zip()
+      |> Enum.map(&Tuple.to_list/1)
+      |> Enum.map(&Enum.reverse/1)
+      |> Enum.map(&apply_operation_2/1)
+      |> Enum.sum()
+
+    {:ok, result}
   end
 end
