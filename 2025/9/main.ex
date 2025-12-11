db44312a8aa3958ea07c4d8ba07bce589588233d
Malhavok <malhavok@origami-dragon.com>
2025-12-11 19:51:43 +0100

    Day 9, 2025 part2 failed

--- 2025/9/main.ex
+++ 2025/9/main.ex
@@ -31,7 +31,141 @@ defmodule Main do
     largest_square(data, 0)
   end
 
-  def part2(_input_data) do
-    {:error, :notimplemented}
+  defp validate_no_non_90deg([], _) do
+    {:ok, :valid}
+  end
+
+  defp validate_no_non_90deg([{x1, y1} = point | tail], {x2, y2}) when x1 == x2 or y1 == y2 do
+    validate_no_non_90deg(tail, point)
+  end
+
+  defp validate_no_non_90deg(_list, _point) do
+    {:error, :invalid}
+  end
+
+  defp build_edges([], _point, edges) do
+    {:ok, edges}
+  end
+
+  defp build_edges([{x1, y1} = point | tail], {x2, y2}, edges) do
+    case x1 == x2 do
+      true ->
+        new_edge = {:x, x1, {min(y1, y2), max(y1, y2)}}
+        build_edges(tail, point, [new_edge | edges])
+
+      false ->
+        new_edge = {:y, y1, {min(x1, x2), max(x1, x2)}}
+        build_edges(tail, point, [new_edge | edges])
+    end
+  end
+
+  defp is_on_edge?(_point, []) do
+    false
+  end
+
+  defp is_on_edge?(point, [{direction, value, {min_entry, max_entry}} | tail]) do
+    edge_points =
+      case direction do
+        :x -> min_entry..max_entry |> Enum.map(fn entry -> {value, entry} end)
+        :y -> min_entry..max_entry |> Enum.map(fn entry -> {entry, value} end)
+      end
+
+    result = edge_points |> Enum.any?(fn elem -> point == elem end)
+
+    if result do
+      true
+    else
+      is_on_edge?(point, tail)
+    end
+  end
+
+  defp count_up_ray_crosses(_ray_start, [], count) do
+    {:ok, count}
+  end
+
+  defp count_up_ray_crosses({x1, y1} = ray_start, [{:y, y_value, {min_x, max_x}} | tail], count) do
+    # Only the y-const values matter when we're thinking about the up-ray.
+
+    if y_value < y1 and min_x <= x1 and x1 <= max_x do
+      count_up_ray_crosses(ray_start, tail, count + 1)
+    else
+      count_up_ray_crosses(ray_start, tail, count)
+    end
+  end
+
+  defp count_up_ray_crosses(ray_start, [_head | tail], count) do
+    count_up_ray_crosses(ray_start, tail, count)
+  end
+
+  defp is_inside_polygon?(point, edges) do
+    {:ok, counter} = count_up_ray_crosses(point, edges, 0)
+    # If we have odd number of hits, it's inside the polygon.
+    rem(counter, 2) == 1
+  end
+
+  defp is_a_good_point?(point, edges) do
+    is_on_edge?(point, edges) or is_inside_polygon?(point, edges)
+  end
+
+  defp is_inside_edges?({direction, value, {min_entry, max_entry}}, edges) do
+    points =
+      case direction do
+        :x -> min_entry..max_entry |> Enum.map(fn entry -> {value, entry} end)
+        :y -> min_entry..max_entry |> Enum.map(fn entry -> {entry, value} end)
+      end
+
+    points
+    |> Enum.all?(fn point -> is_a_good_point?(point, edges) end)
+  end
+
+  defp is_valid_square?({min_x, min_y}, {max_x, max_y}, edges) do
+    working_edges = [
+      {:x, min_x, {min_y, max_y}},
+      {:x, max_x, {min_y, max_y}},
+      {:y, min_y, {min_x, max_x}},
+      {:y, max_y, {min_x, max_x}}
+    ]
+
+    working_edges |> Enum.all?(fn entry -> is_inside_edges?(entry, edges) end)
+  end
+
+  defp largest_square2(_start_point, [], largest_area, _edges) do
+    {:ok, largest_area}
+  end
+
+  defp largest_square2({x1, y1} = start_point, [{x2, y2} | tail], largest_area, edges) do
+    area = (abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+
+    min_x = min(x1, x2)
+    max_x = max(x1, x2)
+    min_y = min(y1, y2)
+    max_y = max(y1, y2)
+
+    # Skip if there's nothing to do.
+    if area <= largest_area || !is_valid_square?({min_x, min_y}, {max_x, max_y}, edges) do
+      largest_square2(start_point, tail, largest_area, edges)
+    else
+      largest_square2(start_point, tail, area, edges)
+    end
+  end
+
+  defp largest_square2([_], largest_area, _edges) do
+    {:ok, largest_area}
+  end
+
+  defp largest_square2([point | tail], largest_area, edges) do
+    {:ok, new_large} = largest_square2(point, tail, largest_area, edges)
+    largest_square2(tail, new_large, edges)
+  end
+
+  def part2(input_data) do
+    data = load_input(input_data)
+
+    # Ensure that there are only basic edges.
+    [head | tail] = data
+    {:ok, :valid} = validate_no_non_90deg(tail, head)
+    {:ok, edges} = build_edges(tail, head, [])
+
+    largest_square2(data, 0, edges)
   end
 end
