5f884e7c61af22090af7c182244002dff11e1536
Malhavok <malhavok@origami-dragon.com>
2025-12-19 20:07:32 +0100

    Smaller path searching

--- 2025/11/main.ex
+++ 2025/11/main.ex
@@ -29,34 +29,52 @@ defmodule Main do
     {:ok, handle1(data, "you")}
   end
 
-  defp handle2(_operations, "out", []) do
+  defp handle2(_operations, target, target) do
     1
   end
 
-  defp handle2(_operations, "out", [_target | _tail]) do
+  defp handle2(_operations, "out", _target) do
     0
   end
 
-  defp handle2(operations, current_node, [target | tail] = targets) do
+  defp handle2(operations, current_node, target) do
     [{^current_node, list_of_outputs}] =
       operations |> Enum.filter(fn {elem, _} -> elem == current_node end)
 
-    if Enum.find(list_of_outputs, fn elem -> elem == target end) do
-      handle2(operations, target, tail)
-    else
-      results =
-        list_of_outputs
-        |> Enum.map(fn output -> handle2(operations, output, targets) end)
-        |> Enum.sum()
-
-      results
-    end
+    list_of_outputs
+    |> Enum.map(fn output -> handle2(operations, output, target) end)
+    |> Enum.sum()
   end
 
   def part2(input_data) do
     data = parse_input(input_data)
 
-    {:ok,
-     handle2(data, "svr", ["fft", "dac", "out"]) + handle2(data, "svr", ["dac", "fft", "out"])}
+    path1 = [
+      handle2(data, "svr", "dac"),
+      handle2(data, "dac", "fft"),
+      handle2(data, "fft", "out")
+    ]
+
+    path1_result =
+      if Enum.any?(path1, fn elem -> elem == 0 end) do
+        0
+      else
+        List.last(path1)
+      end
+
+    path2 = [
+      handle2(data, "svr", "fft"),
+      handle2(data, "fft", "dac"),
+      handle2(data, "dac", "out")
+    ]
+
+    path2_result =
+      if Enum.any?(path2, fn elem -> elem == 0 end) do
+        0
+      else
+        List.last(path2)
+      end
+
+    {:ok, path1_result + path2_result}
   end
 end
