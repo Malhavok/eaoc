d184daebad1314d9dcafdf809b35b333628f0ef5
Malhavok <malhavok@origami-dragon.com>
2025-12-22 16:47:13 +0100

    Printing polyominoes

--- 2025/12/main.ex
+++ 2025/12/main.ex
@@ -19,7 +19,7 @@
 # >>> /opt/homebrew/Cellar/glpk/5.0/bin/glpsol --lp test.lp -o out.txt
 
 defmodule Main.Polyomino do
-  defstruct index: -1, original: %{}, transformed: []
+  defstruct index: -1, original: %{}, x_size: -1, y_size: -1, transformed: []
 
   defp grab_lines(["" | tail], lines) do
     {:ok, lines |> Enum.reverse(), tail}
@@ -33,11 +33,66 @@ defmodule Main.Polyomino do
     {:ok, board_lines, new_tail} = grab_lines(lines, [])
     {:ok, grid} = Grid.init(board_lines |> Enum.join("\n"))
 
+    first_transform =
+      grid
+      |> Map.to_list()
+      |> Enum.filter(fn {_key, value} -> value == ?# end)
+      |> Enum.map(fn {key, _} -> key end)
+      |> Enum.sort()
+      |> MapSet.new()
+
     {
       :ok,
-      %__MODULE__{index: index, original: grid, transformed: []},
+      %__MODULE__{
+        index: index,
+        original: grid,
+        x_size:
+          1 + (grid |> Map.to_list() |> Enum.map(fn {{x_pos, _}, _} -> x_pos end) |> Enum.max()),
+        y_size:
+          1 + (grid |> Map.to_list() |> Enum.map(fn {{_, y_pos}, _} -> y_pos end) |> Enum.max()),
+        transformed: [first_transform]
+      },
       new_tail
     }
+  end
+
+  defp print_polyomio(_, _, y_size, y_size) do
+    IO.puts("")
+    :ok
+  end
+
+  defp print_polyomio(entry, x_size, y_size, y_index) do
+    line_characters =
+      0..x_size
+      |> Enum.map(fn x_index ->
+        key = {x_index, y_index}
+
+        case MapSet.member?(entry, key) do
+          true ->
+            "#"
+
+          false ->
+            " "
+        end
+      end)
+
+    line_characters |> Enum.join("") |> IO.puts()
+
+    print_polyomio(entry, x_size, y_size, y_index + 1)
+  end
+
+  defp print_transform([], _, _) do
+    :ok
+  end
+
+  defp print_transform([head | tail], x_size, y_size) do
+    :ok = print_polyomio(head, x_size, y_size, 0)
+    print_transform(tail, x_size, y_size)
+  end
+
+  @spec print(%__MODULE__{}) :: :ok
+  def print(element) do
+    :ok = print_transform(element.transformed, element.x_size, element.y_size)
   end
 end
 
@@ -83,9 +138,12 @@ defmodule Main do
 
   def part1(input_data) do
     {:ok, polyominoes, boards} = parse_data(input_data)
-    {:ok, polyominoes, boards} |> inspect() |> IO.puts()
 
-    {:error, :notimplemented}
+    [polyomino | _] = polyominoes
+    polyomino |> inspect() |> IO.puts()
+    Main.Polyomino.print(polyomino)
+
+    {:ok, :test}
   end
 
   def part2(_input_data) do
