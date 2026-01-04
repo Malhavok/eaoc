3a3d3b016b08cf3f827c8b71a65ba03acdb75816
Malhavok <malhavok@origami-dragon.com>
2026-01-04 17:38:37 +0100

    Day 5, 2016, part2 done

--- 2016/5/main.ex
+++ 2016/5/main.ex
@@ -20,7 +20,35 @@ defmodule Main do
     calc_hash(input_str, 0, [])
   end
 
-  def part2(_input_data) do
-    {:error, :notimplemented}
+  defp calc_hash2(input_str, index, result) do
+    hash_input = "#{input_str}#{index}"
+    hash_output = :crypto.hash(:md5, hash_input) |> Base.encode16() |> String.downcase()
+
+    next_letter_index = hash_output |> String.at(5) |> String.to_integer(16)
+
+    if hash_output |> String.byte_slice(0, 5) == "00000" and
+         next_letter_index <= 7 and
+         !Map.has_key?(result, next_letter_index) do
+      next_letter = hash_output |> String.at(6)
+      new_result = Map.put(result, next_letter_index, next_letter)
+
+      if new_result |> Enum.count() == 8 do
+        {:ok,
+         new_result
+         |> Map.to_list()
+         |> Enum.sort(:asc)
+         |> Enum.map(fn {_, val} -> val end)
+         |> Enum.join("")}
+      else
+        calc_hash2(input_str, index + 1, new_result)
+      end
+    else
+      calc_hash2(input_str, index + 1, result)
+    end
+  end
+
+  def part2(input_data) do
+    input_str = input_data |> String.split("\n") |> Enum.at(0)
+    calc_hash2(input_str, 0, %{})
   end
 end
