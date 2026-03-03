46ca1f10e0ecc22f9b7d6307c24cd81e4676a3d3
Malhavok <malhavok@origami-dragon.com>
2026-03-03 19:15:58 +0100

    Day 13, 2016, part1 done

--- 2016/13/main.ex
+++ 2016/13/main.ex
@@ -31,6 +31,10 @@ defmodule Main do
     rem(num_ones, 2) == 1
   end
 
+  defp is_point_wall?(%Point2D{x: x, y: y}, _number) when x < 0 or y < 0 do
+    true
+  end
+
   defp is_point_wall?(point, number) do
     case :ets.lookup(@grid_cache, point) do
       [{^point, is_wall}] ->
@@ -43,10 +47,54 @@ defmodule Main do
     end
   end
 
+  defp try_move_from_point(current_point, distance, number, priority_queue, visited) do
+    if MapSet.member?(visited, current_point) do
+      {priority_queue, visited}
+    else
+      move_from_point(current_point, distance, number, priority_queue, visited)
+    end
+  end
+
+  defp move_from_point(current_point, distance, number, priority_queue, visited) do
+    next_distance = distance + 1
+
+    new_queue =
+      Point2D.carinal()
+      |> Enum.map(fn direction -> Point2D.add(current_point, direction) end)
+      |> Enum.filter(fn position -> !is_point_wall?(position, number) end)
+      |> Enum.filter(fn position -> !MapSet.member?(visited, position) end)
+      |> Enum.reduce(priority_queue, fn position, acc ->
+        PriorityQueue.put(acc, {next_distance, position})
+      end)
+
+    # Since we always go from the "smallest" value, we don't really need to keep distances here.
+    new_visited = MapSet.put(visited, current_point)
+
+    {new_queue, new_visited}
+  end
+
+  defp search_for_target(start_point, end_point, number) do
+    queue = PriorityQueue.new() |> PriorityQueue.put({0, start_point})
+    run_search(end_point, number, queue, MapSet.new())
+  end
+
+  defp run_search(end_point, number, priority_queue, visited) do
+    {{distance, current_point}, new_queue} = PriorityQueue.pop(priority_queue)
+
+    if current_point == end_point do
+      {:ok, distance}
+    else
+      {bumped_queue, new_visited} =
+        try_move_from_point(current_point, distance, number, new_queue, visited)
+
+      run_search(end_point, number, bumped_queue, new_visited)
+    end
+  end
+
   def part1(input_data) do
     {:ok, magic_number, end_position} = parse_input(input_data)
     :ets.new(@grid_cache, [:named_table, :set])
-    {:ok, :test}
+    {:ok, _distance} = search_for_target(Point2D.new(1, 1), end_position, magic_number)
   end
 
   def part2(_input_data) do
